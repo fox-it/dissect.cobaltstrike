a2660a6f1fb7f257ed74ed5b7f2e52f394d0733c
Yun Zheng Hu <hu@fox-it.com>
2024-10-14 07:30:21 +0000

    Fix BeaconSetting names with unknown values

--- dissect/cobaltstrike/beacon.py
+++ dissect/cobaltstrike/beacon.py
@@ -794,7 +794,7 @@ class BeaconConfig:
         for setting in self.settings_tuple:
             val = setting.value
             if index_type == "name":
-                key = setting.index.name or str(setting.index)
+                key = setting.index.name or str(setting.index).replace(".", "_")
             elif index_type == "const":
                 key = setting.index.value
             else:
